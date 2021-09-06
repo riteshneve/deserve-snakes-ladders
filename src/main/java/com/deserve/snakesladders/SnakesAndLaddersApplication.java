7a1709d6c93d2d1aea6c8385206a755700fa20f0
Ritesh Neve <ritesh.n@turtlemint.com>
2021-09-06 12:04:08 +0530

    first commit

---
+++ src/main/java/com/deserve/snakesladders/SnakesAndLaddersApplication.java
@@ -0,0 +1,13 @@
+package com.deserve.snakesladders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakesAndLaddersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnakesAndLaddersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deserve/snakesladders/SnakesAndLaddersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deserve.snakesladders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakesAndLaddersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
